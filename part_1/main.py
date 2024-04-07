660806b7314a3e05bf03b7b1c34c89afca9a119a
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-04-07 11:37:58 +0200

    Implement debt splitting algorithm

--- part_1/main.py
+++ part_1/main.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import islice
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
@@ -15,14 +16,46 @@ class Transaction:
 def load_transactions(absolute_path: str) -> list[Transaction]:
     with open(absolute_path) as f:
         reader = csv.reader(f)
-        data = list(reader)
+        transactions = list(reader)
 
     transaction_list: list[Transaction] = []
 
-    for entry in data:
-        transaction_list.append(Transaction(entry[0], entry[1], int(entry[2])))
+    for transaction in transactions:
+        user_from, user_to, amount = transaction
+        transaction_list.append(Transaction(user_from, user_to, int(amount)))
 
     return transaction_list
+
+
+def calculate_balance(transaction_list: list[Transaction]) -> dict[str, int]:
+    balance_dict: dict[str, int] = {}
+    for transaction in transaction_list:
+        if transaction.user_from not in balance_dict:
+            balance_dict[transaction.user_from] = 0
+        if transaction.user_to not in balance_dict:
+            balance_dict[transaction.user_to] = 0
+
+        balance_dict[transaction.user_from] += transaction.amount
+        balance_dict[transaction.user_to] -= transaction.amount
+
+    return balance_dict
+
+
+def calculate_transactions(balance_dict: dict[str, int]) -> list[Transaction]:
+    transactions: list[Transaction] = []
+    for i, (name, amount) in islice(
+        enumerate(balance_dict.items()), 0, len(balance_dict) - 1
+    ):
+        for n in islice(balance_dict, i + 1, len(balance_dict)):
+            a = balance_dict[n]
+            if amount * a < 0:
+                if amount < a:
+                    transactions.append(Transaction(name, n, abs(amount)))
+                else:
+                    transactions.append(Transaction(n, name, abs(amount)))
+                balance_dict[n] += amount
+                break
+    return transactions
 
 
 if __name__ == "__main__":
@@ -35,7 +68,10 @@ if __name__ == "__main__":
     )
 
     filename = parser.parse_args().filename
+    # filename = "/home/bartek/projects/ocado-cloud-recruitment/test_data/debts_1.csv"
 
     transactions = load_transactions(filename)
-    for t in transactions:
+    balance = calculate_balance(transactions)
+    new_transactions = calculate_transactions(balance)
+    for t in new_transactions:
         print(t)
